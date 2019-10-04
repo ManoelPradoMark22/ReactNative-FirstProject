537b7a75e6e8c9c02acabdb0c438e6a4ce8dd07b
Manoel de Oliveira Prado Neto <manoelprado.aecjr@gmail.com>
2019-10-04 11:57:50 -0300

    ReactNavigation, StatusBarConfigure

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,9 +1,7 @@
 import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-  /* retorna true qnd o usuario esta emulando aplicacao em ambiente de
-  desenvolvimento, ou seja, td aqui dentro nao vai executar em amb de producao */
-  const tron = Reactotron.configure({ host: '192.168.114.17' }) // ip do pc
+  const tron = Reactotron.configure({ host: '172.17.192.1' }) // ip do pc
     .useReactNative()
     .connect();
 
@@ -11,20 +9,8 @@ if (__DEV__) {
 
   tron.clear();
   /* Limpar a timeline do tron sempre que dou um refresh na aplicacao
-  é opcional, caso nao queira é só remover aqui */
+  é opcional, caso nao queira é só remover aqui
+  Atenção! no host usei o ip 10.0.0.161 para o dispositivo físico
+  e o ip 172.17.192.1 para o emulador!
+  basta, no cmd, executar ipconfig q suas infos de ip irão aparecer */
 }
-
-/*
-caso esteja emulando atraves do usb:
-const tron = Reactotron.configure({host: 'ip_do_computador'})
-    .useReactNative()
-    .connect();
-
-Caso vc esteja no android e mesmo fazendo o passo anterior ainda assim
-nada apareceu no reactoton, precisaremos fazer um redirecionamento de portas.
-execute na raiz do projeto: add reverse tcp:9090 tcp:9090
-se o adb nao estiver configurado no path do seu pc, execute esse codigo ao invés
-do outro, mas primeiro execute um cd para chegar ao diretório:
-cd C:\Users\manoe\AppData\Local\Android\Sdk\platform-tools
-e entao execute:
-adb reverse tcp:9090 tcp:9090 */
---
+++ src/routes.js
@@ -0,0 +1,28 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+/* Routes vai ser um componente por isso letra maiúscula
+o createAppContainer() ele deve vir por volta de todas as rotas */
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+      User,
+    },
+    {
+      headerLayoutPreset: 'center',
+      headerBackTitleVisible: false, // so a flecha de voltar aparece, a palavra voltar nao
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#7159c1',
+        },
+        headerTintColor: '#fff',
+      },
+    }
+  )
+);
+
+export default Routes;
