a1dc611165e34538d699bab795529ba771fe7330
Manoel de Oliveira Prado Neto <manoelprado.aecjr@gmail.com>
2019-10-04 13:27:19 -0300

    styling form

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 30px;
+`;
+
+/* Não temos um componente de Form no RN entao
+utilizaremos uma View comum */
+export const Form = styled.View`
+  flex-direction: row;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#999',
+})`
+  flex: 1; /*ocupa toda largura possivel, fora a
+  largura do proprio botão */
+  height: 40px;
+  background: #eee;
+  border-radius: 4px;
+  padding: 0 15px;
+  border: 1px solid #eee;
+`;
+
+/* Vamos utilizar o botao RectButton do
+react-native-gesture-handler pq ele se adapta
+com as peculiaridades do android e do ios */
+export const SubmitButton = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  background: #7159c1;
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 0 12px;
+`;
